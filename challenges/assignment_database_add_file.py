542dc1b968be6a7687a0523e7d29c2cf13135a44
cmoody2 <cmoody.dev@gmail.com>
2020-11-07 00:02:40 -0800

    Update assignment_database_add_file.py

--- challenges/assignment_database_add_file.py
+++ challenges/assignment_database_add_file.py
@@ -5,8 +5,12 @@ import sqlite3
 fileList = ('information,docx', 'Hello.txt', 'myImage.png', \
             'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
 
-conn = sqlite3.connect('file.db')
+# ----------------------------------------------------------------
+# This creates our database named file.db with two columns:
+# ID(which will be the primary key) and FileName
+# ----------------------------------------------------------------
 
+conn = sqlite3.connect('file.db')
 
 with conn:
     cursor = conn.cursor()
@@ -19,6 +23,11 @@ conn.close()
 
 
 
+# ----------------------------------------------------------------
+# This iterates through the file list and grabs each file
+# with the .txt type and enters them into the table under FileName
+# ----------------------------------------------------------------
+print("Search results for .txt files: \n")
 
 for i in fileList:
     if '.txt' in i:
@@ -30,5 +39,25 @@ for i in fileList:
                            (fileName))
             conn.commit()
         conn.close()
+
+
+
+# ----------------------------------------------------------------
+# This is our query to retrieve all records under column 'FileName'
+# ----------------------------------------------------------------
+conn = sqlite3.connect('file.db')
+
+with conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT FileName FROM Txt_Files")
+    varFiles = cursor.fetchall()
+    for item in varFiles:
+        msg = "File Name: {}\n".format(item[0])
+    print(msg)
+conn.close()
+
+
+
+
 
 
