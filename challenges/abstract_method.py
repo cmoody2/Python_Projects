feab60fccb699d974371a87626db6dbe1c43c138
cmoody2 <cmoody.dev@gmail.com>
2020-11-12 23:34:41 -0800

    Update abstract_method.py

--- challenges/abstract_method.py
+++ challenges/abstract_method.py
@@ -17,6 +17,7 @@ class Leaderboard(ABC):
     def playerScore(self, score):
         print("Your score is: ",score)
 
+    # Creates an abstract method that a child class can define and use
     @abstractmethod
     def insertScore(self, score):
         pass
@@ -27,9 +28,12 @@ class TetrisScore(Leaderboard):
     def insertScore(self, score):
         user = "SAM"
         lboard = {"DEX": 4500, "WIL": 4299, "FRD": 4001, "GBL": 3990, "ADM": 4490}
+        # Adds new entry into dictionary
         lboard.update({user: score})
+        # Sorts dictionary by value in ascending order
         newlboard = sorted(lboard.items(), key = lambda kv:(kv[1], kv[0]))
         print("\nCurrent leader board:")
+        # For loop to print values in descending order
         for k, v in reversed(newlboard):
             print(k, v)
 
