44f5176071e8805e167a9bedfaa92a194e18e4bd
cmoody2 <cmoody.dev@gmail.com>
2020-11-15 14:37:47 -0800

    Create check_files_func.py

---
+++ challenges/Check files GUI Challenge/check_files_func.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#   Python ver:     3.9.0
+#
+#   Author:         Christopher A. Moody
+#
+#   Purpose:        Create a gui using Tkinter module
+#
+#   Tested OS:      Written and tested with Windows 10.
+
+
+import os
+from tkinter import *
+import tkinter as tk
+
+
+def center_window(self, w, h):  # Pass in the tkinter frame (master) reference and the w and h
+    # Get user's screen width and height
+    screen_width = self.master.winfo_screenwidth()
+    screen_height = self.master.winfo_screenheight()
+    # Calculate x and y coordinates to paint the app centered on the user's screen
+    x = int((screen_width/2) - (w/2))
+    y = int((screen_height/2) - (h/2))
+    centerGeo = self.master.geometry('{}x{}+{}+{}'.format(w, h, x, y))
+    return centerGeo
+
+
+def ask_quit(self):
+    if messagebox.askokcancel("Exit program", "Okay to exit application?"):
+        # This closes app
+        self.master.destroy()
+        os._exit(0)
+
+
+
+if __name__ == "__main__":
+    pass
