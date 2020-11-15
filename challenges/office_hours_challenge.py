30a9ffc843cae6744f30d0e58002cd58871c0748
cmoody2 <cmoody.dev@gmail.com>
2020-11-15 13:35:17 -0800

    Update office_hours_challenge.py

--- challenges/office_hours_challenge.py
+++ challenges/office_hours_challenge.py
@@ -3,35 +3,48 @@ from pytz import timezone
 
 
 def Portlandtime():
-    fmt = "%H:%M:%S %Z%z"
+    fmt = "%H%M"
     now_time = datetime.now(timezone('US/Pacific'))
-    return now_time.strftime(fmt)
+    now_hour = int(now_time.strftime(fmt))
+    if now_hour >= 900 and now_hour <= 1700:
+        print("HQ (Portland) is currently open")
+    else:
+        print("HQ (Portland) is currently closed")
+    return now_hour
 
 
 def Nytime():
-    fmt = "%H:%M:%S %Z%z"
+    fmt = "%H%M"
     now_time = datetime.now(timezone('US/Eastern'))
-    return now_time.strftime(fmt)
-
+    now_hour = int(now_time.strftime(fmt))
+    if now_hour >= 900 and now_hour <= 1700:
+        print("New York Branch is currently open")
+    else:
+        print("New York Branch is currently closed")
+    return now_hour
 
 
 def Londontime():
-    fmt = "%H:%M:%S %Z%z"
+    fmt = "%H%M"
     now_time = datetime.now(timezone('Europe/London'))
-    return now_time.strftime(fmt)
-
+    now_hour = int(now_time.strftime(fmt))
+    if now_hour >= 900 and now_hour <= 1700:
+        print("London Branch is currently open")
+    else:
+        print("London is currently closed")
+    return now_hour
 
 
 
 
 
 if __name__ == "__main__":
+    hq = Portlandtime()
     ny_office = Nytime()
     london_office = Londontime()
-    hq = Portlandtime()
-    print(ny_office)
-    print(london_office)
-    print(hq)
+
+    
+    
     
 
 
