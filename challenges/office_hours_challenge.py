645fdf6d65ec32cfda6c13f0cf3801c9e42e0cb6
cmoody2 <cmoody.dev@gmail.com>
2020-11-15 13:44:10 -0800

    Update office_hours_challenge.py
    - add comments

--- challenges/office_hours_challenge.py
+++ challenges/office_hours_challenge.py
@@ -1,11 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#   Python ver:     3.9.0
+#
+#   Author:         Christopher A. Moody
+#
+#   Purpose:        Create a program to check if locations are open of a
+#                   Portland-based company with branches in New York and
+#                   London. Demonstrates Use of datetime and pytz packages.
+#                   
+#
+#   Tested OS:      Written and tested with Windows 10.
+
+
 from datetime import datetime, timedelta, time
 from pytz import timezone
 
-
+# Portland HQ time and hours
 def Portlandtime():
+    # first we format time to military format
     fmt = "%H%M"
+    # now we get the current time in regards to specified time zone
+    # this is considered an 'aware' time object
     now_time = datetime.now(timezone('US/Pacific'))
+    # next we convert current time into an int using our
+    # specified format
     now_hour = int(now_time.strftime(fmt))
+    # then we check if given time fits with the
+    # window of being open with an 'if' statment
     if now_hour >= 900 and now_hour <= 1700:
         print("HQ (Portland) is currently open")
     else:
@@ -13,6 +34,7 @@ def Portlandtime():
     return now_hour
 
 
+# New York time and hours
 def Nytime():
     fmt = "%H%M"
     now_time = datetime.now(timezone('US/Eastern'))
@@ -24,6 +46,7 @@ def Nytime():
     return now_hour
 
 
+# London time and hours
 def Londontime():
     fmt = "%H%M"
     now_time = datetime.now(timezone('Europe/London'))
@@ -33,8 +56,6 @@ def Londontime():
     else:
         print("London is currently closed")
     return now_hour
-
-
 
 
 
