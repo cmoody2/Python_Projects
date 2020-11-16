251ecd545f43ab93d9edf79301c83f23b5eee34b
cmoody2 <cmoody.dev@gmail.com>
2020-11-15 16:38:43 -0800

    Update check_files_main.py

--- challenges/Check files GUI Challenge/check_files_main.py
+++ challenges/Check files GUI Challenge/check_files_main.py
@@ -16,26 +16,28 @@ from tkinter import messagebox
 import check_files_func
 import check_files_gui
 
-
+# our main gui class
 class ParentWindow(Frame):
     def __init__(self, master, *args, **kwargs):
         Frame.__init__(self, master, *args, **kwargs)
 
         self.master = master
-        self.master.minsize(300,500)
-        self.master.maxsize(300,500)
+        self.master.minsize(500,170)  #width,height
+        self.master.maxsize(500,170)  #width,height
 
-        check_files_func.center_window(self, 300, 500)
+        check_files_func.center_window(self, 500, 170)
         self.master.title("Check files")
         self.master.configure(bg="#F0F0F0")
 
+        #protocol for a usable close button in the gui itself that will
+        #ask before closing using a popup window
         self.master.protocol("WM_DELETE_WINDOW", lambda: check_files_func.ask_quit(self))
         arg = self.master
-
         check_files_gui.load_gui(self)
 
 
 if __name__ == "__main__":
+    #this makes sure the window stays open
     root = tk.Tk()
     App = ParentWindow(root)
     root.mainloop()
